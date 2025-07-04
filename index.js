20792d8c409729c03e9e832e436c5021a9109766
arunsomu23 <arun.smartarun@gmail.com>
2025-07-04 15:28:50 -0300

    delpoy render

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const patientRoutes = require('./routes/patientRoutes');
+const authRoutes = require('./routes/authRoutes');
+const PatientListRoutes = require('./routes/PatientListRoutes');
+const errorHandler = require('./middlewares/errorHandler');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Patient CRUD API is running');
+});
+
+
+app.use('/api/patients', patientRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/patientList', PatientListRoutes);
+
+// Error-handling middleware
+app.use(errorHandler);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running at http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch(err => console.error(err));
+
+  module.exports = app;
