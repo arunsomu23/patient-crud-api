e6ff956227ed69b5d54533fe73d1189a5a586827
arunsomu23 <arun.smartarun@gmail.com>
2025-07-04 14:49:14 -0300

    delpoy vercel

---
+++ app/page.js
@@ -0,0 +1,9 @@
+// app/page.js
+export default function Home() {
+    return (
+      <main>
+        <h1>Welcome to Patient CRUD App</h1>
+      </main>
+    );
+  }
+  
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to Patient CRUD App</h1>
+      </div>
+    );
+  }
+  
